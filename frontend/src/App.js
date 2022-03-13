8222ddedcb2ae7f5609c34aec3b3c960e6b7eabc
leesh5000 <leesh50000@gmail.com>
2022-03-13 23:41:02 +0900

    add frontend project

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import NotFound from './pages/NotFound';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
+  );
+};
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+
+const Home = () => {
+  return (
+    <>
+      <HeaderContainer>
+        <div>Hello</div>
+      </HeaderContainer>
+    </>
+  );
+};
+
+export default Home;
